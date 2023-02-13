19ccc9b1314f29faa7b4486705f0bf41bc1c3384
Loise <loise.wambui@student.moringaschool.com>
2023-02-13 21:14:03 +0300

    commit tree efe3e6b1cbed644ce5258fa26eba662b973250eb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = []
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarve, index|
+    puts "#{index + 1}.#{dwarve}"
+  end
 end
+roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |call|
+    pp "#{call}!".capitalize
+  end
 end
+summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+calls = ["puffs", "go", "two"]
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
+puts long_planeteer_calls(calls)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+cheeses = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese = if cheeses.find(cheese_types[0])
+              cheese_types[0]
+           elsif cheeses.find(cheese_types[1])
+              cheese_types[1]
+           elsif cheeses.find(cheese_types[2])
+              cheese_types[2]
+           else 
+             nil
+          end
 end
+puts find_the_cheese(cheeses)
